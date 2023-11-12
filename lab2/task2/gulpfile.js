a057780188c9410d47e6dce4e9eb625ea352196e
RomanKharkovskoy <roman.kharkovskoy@yandex.ru>
2023-11-12 21:03:00 +0300

    added comments

--- lab2/task2/gulpfile.js
+++ lab2/task2/gulpfile.js
@@ -5,7 +5,10 @@ const cssnano = require('cssnano');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
-// Sass Task
+// Таск для компиляии Sass:
+// Эта функция берет исходный файл Sass (style.scss), 
+// компилирует его в CSS, применяет PostCSS с плагином для минификации (cssnano) 
+// и сохраняет результат в папку dist с использованием sourcemaps.
 function scssTask(){
   return src('src/scss/style.scss', { sourcemaps: true })
     .pipe(sass())
@@ -14,13 +17,17 @@ function scssTask(){
 }
 
 // JavaScript Task
+// Эта функция берет исходный файл JavaScript (main.js), 
+ // минифицирует его с использованием Terser 
+ // и сохраняет результат в папку dist с sourcemaps.
 function jsTask(){
   return src('src/js/main.js', { sourcemaps: true })
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
-// Browsersync Tasks
+// Browsersync таск
+// Функция инициализирует Browsersync и запускает сервер
 function browsersyncServe(cb){
   browsersync.init({
     server: {
@@ -30,18 +37,25 @@ function browsersyncServe(cb){
   cb();
 }
 
+// Функция выполняет перезагрузку браузера
 function browsersyncReload(cb){
   browsersync.reload();
   cb();
 }
 
 // Watch Task
+// Эта функция отслеживает изменения в файле index.html 
+// и в файлах с расширениями .scss и .js в папках src/scss и src/js. 
+// При изменении запускаются соответствующие задачи (scssTask, jsTask) 
+// и происходит перезагрузка через Browsersync.
 function watchTask(){
   watch('index.html', browsersyncReload);
   watch(['src/scss/*.scss', 'src/js/*.js'], series(scssTask, jsTask, browsersyncReload));
 }
 
-// Default Gulp task
+// Главная функция
+// Функция включает в себя выполнение задач по компиляции Sass, 
+// обработке JavaScript, запуску сервера Browsersync и отслеживанию изменений.
 exports.openProject = series(
   scssTask,
   jsTask,
